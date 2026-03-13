0973b6315530ce11a4a9464b9e629ee5fad2320f
juratovorif-web <juratovorif@gmail.com>
2026-03-14 00:55:08 +0500

    work

---
+++ 3_8_dan_3_11_gaca/src/4_1_Exceptions/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _4_1_Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            Thread thread1 = new Thread(Do1);
+            thread1.Start();
+            Thread thread2 = new Thread(Do2);
+            thread2.Start();
+            
+           
+        }
+        static void Do1()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+               Console.WriteLine("Hello");
+            }
+        }
+        static void Do2()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+               Console.WriteLine("Word");
+            }
+        }
+                
+
+    }
+       
+
+
+}
+
+
+       
+
+
+                   
+            
+               
+              
+
+
+
+
+          
