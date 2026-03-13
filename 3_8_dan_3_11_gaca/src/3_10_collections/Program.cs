ea9e7e8068f644004463f2bec33b4d5a994beee6
juratovorif-web <juratovorif@gmail.com>
2026-03-14 00:53:59 +0500

    work

---
+++ 3_8_dan_3_11_gaca/src/3_10_collections/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _3_10_collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
