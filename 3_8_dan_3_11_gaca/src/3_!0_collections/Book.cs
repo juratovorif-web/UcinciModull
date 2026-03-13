548f6fa40f79f163f0f99a9f9405d5c753451d58
juratovorif-web <juratovorif@gmail.com>
2026-03-14 00:53:14 +0500

    dars

---
+++ 3_8_dan_3_11_gaca/src/3_!0_collections/Book.cs
@@ -0,0 +1,7 @@
+﻿namespace _3__0_collections;
+
+public class Book
+{
+    public string Name { get; set; }
+    public string Autor { get; set; }
+}
---
+++ 3_8_dan_3_11_gaca/src/3_!0_collections/Program.cs
@@ -0,0 +1,134 @@
+﻿namespace _3__0_collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+            // 3
+            //Stack<int> stack = new Stack<int>();
+            //stack.Push(77);
+            //stack.Push(47);
+            //stack.Push(58);
+            //stack.Push(41);
+            //var max = stack.Max();
+            //foreach(var item  in stack)
+            //{
+            //    if(item >  max)
+            //    {
+            //        max = item; 
+            //    }
+            //}
+            //Console.WriteLine(max);
+
+
+
+
+
+
+            // 2
+            //Stack<char> stack = new Stack<char>();
+            //var str = "salom";
+            //for(var i  = 0; i < str.Length; i++)
+            //{
+            //    stack.Push(str[i]);
+            //}
+            //while(stack.Count > 0)
+            //{
+            //    Console.Write(stack.Pop());
+            //}
+
+
+            // 1
+            //Stack<int> stack = new Stack<int>();    
+            //stack.Push(1);
+            //stack.Push(2);
+            //stack.Push(3);
+            //stack.Push(4);
+            //stack.Push(5);
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack.Pop());
+
+            //int a = 5;
+            //Console.WriteLine(a.GetHashCode());
+            //Console.WriteLine(a.GetHashCode());
+
+
+            //string s1 = "salom";
+            //string s2 = "salom";
+            //Console.WriteLine(s1.GetHashCode()); // 1785588553
+            //Console.WriteLine(s1.GetHashCode()); // 1785588553 keyi brxl value si br xl boganiga 
+            //cunki 2 ta br xl value bor dvoti
+            // agar value si ozgarsa hash codi ozgaradi unic boladi
+
+
+
+
+            //Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
+
+            //keyValuePairs.Add(4, "salom");
+            //keyValuePairs.Add(8, "salom");
+            //keyValuePairs.Add(9, "hello");
+            //keyValuePairs.Add(2, "salom");
+            //keyValuePairs.Add(8, "privet");
+
+            //keyValuePairs.Remove(2);
+
+            //Console.WriteLine(keyValuePairs.GetValueOrDefault(18));
+
+            //foreach (var item in keyValuePairs)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+            //keyValuePairs.Add(1, 2);
+
+            //Console.WriteLine(keyValuePairs.GetValueOrDefault(4));
+
+
+
+
+
+            //Book book = new Book() { Name = "Mr Robot", Autor = "Eliyot" };
+            //Book book1 = new Book() { Name = "Club 5 Morning", Autor = "Robbin Sharma" };
+            //Book book3 = new Book() { Name = "Second Foundation", Autor = "Bekzod" };
+            //Book book4 = new Book() { Name = "Foundation", Autor = "Izzatullo" };
+            //Book book5 = new Book() { Name = "Foundation", Autor = "Izzatullo" };
+
+            //Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            //keyValuePairs.Add(book.Name, book.Autor);
+            //keyValuePairs.Add(book1.Autor.ToString(), book1.Name);
+
+            //Console.WriteLine(keyValuePairs.Last());
+
+            //Stack<int> stack = new Stack<int>();
+            //Random random = new Random();
+            //var res = 0;
+            //for(var i = 0; i < 10; i++)
+            //{
+            //    res = random.Next(10);
+            //    stack.Push(res);
+            //}
+            //foreach (var item in stack)
+            //{
+            //    if (item % 2 == 0)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+        }
+    }
+}
